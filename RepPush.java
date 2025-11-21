87732a0a69d986d6979d5e304aad8368d025c3e5
Prasbin <np01ai4a250148@islingtoncollege.edu.np>
2025-11-21 07:49:27 +0545

    I am your boss.

---
+++ RepPush.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class RepPush here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class RepPush
+{
+    public static void main(String[] args)
+    {
+        System.out.println("I am your boss");
+    }
+}
